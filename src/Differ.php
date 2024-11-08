549d013c4bf47dcaabf8897d0dc0231a4111ac9a
semyanskikh-im <semyanskikh@gmail.com>
2024-11-08 17:08:50 +0300

    try to make differ

---
+++ src/Differ.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace GenerateDiff\Differ;
+
+use function Functional\sort;
+
+function differ($array1, $array2, $depth = 0)
+{
+    $keys = array_unique(array_merge(array_keys($array1), array_keys($array2)));//получаем все ключи 2 массивов на одной глубине
+
+    $sortedKeys = sort($keys,//сортируем по алфавиту
+        function ($a, $b) {
+            return strcmp($a, $b);
+        });
+
+    print_r($sortedKeys);    
+
+    $result = array_map(function($key) use ($array1, $array2, $depth) {
+        //если ключ есть только в первом массиве
+        if (array_key_exists($key, $array1) && (!array_key_exists($key, $array2))) {
+            $diff[] = ['depth' => $depth, 'status' => '-', 'children' => [$key => $array1[$key]]];
+            return $diff;
+        } 
+        //если ключ есть только во втором массиве
+        if (!array_key_exists($key, $array1) && (array_key_exists($key, $array2))) {
+            $diff[] = ['depth' => $depth, 'status' => '+', 'children' => [$key => $array2[$key]]];
+            return $diff;
+        } 
+        
+        //если ключ есть в обоих массивах:
+        if (array_key_exists($key, $array1) && (array_key_exists($key, $array2))) {
+        //1. значения одинаковы:
+            if ($array1[$key] === $array2[$key]) {
+                $diff[] = ['depth' => $depth, 'status' => ' ', 'children' => [$key => $array1[$key]]];
+                return $diff;
+            }
+
+        //2. значения разные: 
+          // если одно или второе значение не является массивом, то просто записываем разность
+            if ((!is_array($array1[$key])) || (!is_array($array2[$key]))) {
+
+                $diff[] = ['depth' => $depth, 'status' => '-', 'children' => [$key => $array1[$key]]];
+                $diff[] = ['depth' => $depth, 'status' => '+', 'children' => [$key => $array2[$key]]];
+                
+                return $diff;
+            }
+
+            //если оба массивы...
+            if (is_array($array1[$key]) && is_array($array2[$key])) {
+                // ... и оба ассоциативные, проверяем детей на уровень ниже
+                if (array_is_list($array1[$key]) && array_is_list($array2[$key])) {
+                        
+                    return $diff[] = ['depth' => $depth, 'status' => ' ', 'children' => [$key => differ($array1[$key], $array2[$key], $depth + 1)]];
+                } else {//в противном случае, возвращаем как есть
+
+                    $diff[] = ['depth' => $depth, 'status' => '-', 'children' => [$key => $array1[$key]]];
+                    $diff[] = ['depth' => $depth, 'status' => '+', 'children' => [$key => $array2[$key]]];
+                    return $diff;
+                }
+            }
+        }
+        }, $sortedKeys);
+
+    return $result;
+}
--- src/Gendiff.php
+++ src/Gendiff.php
@@ -3,6 +3,7 @@
 namespace GenerateDiff\Gendiff;
 
 use function GenerateDiff\Parsers\parse;
+use function GenerateDiff\Differ\differ;
 use function GenerateDiff\Functions\setValueToString;
 use function Functional\sort;
 
@@ -17,50 +18,61 @@ function genDiff($filename1, $filename2)
     $firstParserResult = parse($firstFilePath); //данные парсинга первого файла
     $secondParserResult = parse($secondFilePath);//данные парcинга второго файла
 
-    //var_export($firstParserResult) . PHP_EOL;
-    //var_export($secondParserResult) . PHP_EOL;
+    // var_export($firstParserResult) . PHP_EOL;
+    // var_export($secondParserResult) . PHP_EOL;
 
     //проходимся по данным первого массива(файла):
-    foreach ($firstParserResult as $k => $v) {
-        if (array_key_exists($k, $secondParserResult)) {//если ключ есть в обоих массивах, то 2 варианта:
-            if ($firstParserResult[$k] === $secondParserResult[$k]) {// 1. если зачение в обоих массивах одинаковое
-                $difference[] = ['sign' => ' ', 'key' => $k, 'value' => setValueToString($v)];
-            } else {// 2. если значение изменилось
-                $difference[] = ['sign' => '-', 'key' => $k, 'value' => setValueToString($v)];
-                $difference[] = ['sign' => '+', 'key' => $k, 'value' => setValueToString($secondParserResult[$k])];
-            }
-        } else {//если ключ есть только в первом массиве
-            $difference[] = ['sign' => '-', 'key' => $k, 'value' => setValueToString($v)];
-        }
-    }
+    // foreach ($firstParserResult as $k => $v) {
+    //     if (array_key_exists($k, $secondParserResult)) {//если ключ есть в обоих массивах, то 2 варианта:
+    //         if ($firstParserResult[$k] === $secondParserResult[$k]) {// 1. если зачение в обоих массивах одинаковое
+    //             $difference[] = ['sign' => ' ', 'key' => $k, 'value' => setValueToString($v)];
+    //         } else {// 2. если значение изменилось
+    //             $difference[] = ['sign' => '-', 'key' => $k, 'value' => setValueToString($v)];
+    //             $difference[] = ['sign' => '+', 'key' => $k, 'value' => setValueToString($secondParserResult[$k])];
+    //         }
+    //     } else {//если ключ есть только в первом массиве
+    //         $difference[] = ['sign' => '-', 'key' => $k, 'value' => setValueToString($v)];
+    //     }
+    // }
 
     //проходим по данным второго массива:
-    foreach ($secondParserResult as $k => $v) {
-        if (!array_key_exists($k, $firstParserResult)) {//если ключ-значение есть только во втором массиве
-             $difference[] = ['sign' => '+', 'key' => $k, 'value' => setValueToString($v)];
-        }
-    }
+    // foreach ($secondParserResult as $k => $v) {
+    //     if (!array_key_exists($k, $firstParserResult)) {//если ключ-значение есть только во втором массиве
+    //          $difference[] = ['sign' => '+', 'key' => $k, 'value' => setValueToString($v)];
+    //     }
+    // }
 
     //сортируем массив различий, чтобы значения по ключу 'key' были в алфавитном порядке:
-    $sortedDifference = sort(
-        $difference,
-        function ($a, $b) {
-            return strcmp($a['key'], $b['key']);
-        }
-    );
+    // $sortedDifference = sort(
+    //     $difference,
+    //     function ($a, $b) {
+    //         return strcmp($a['key'], $b['key']);
+    //     }
+    // );
 
     //формируем массив из строк для финального вывода:
-    $stringDifference = array_map(
-        function ($line) {
-            return "  {$line['sign']} {$line['key']}: {$line['value']}\n";
-        },
-        $sortedDifference
-    );
+    // $stringDifference = array_map(
+    //     function ($line) {
+    //         return "  {$line['sign']} {$line['key']}: {$line['value']}\n";
+    //     },
+    //     $sortedDifference
+    // );
 
-    $stringDifference = implode($stringDifference);// преобразуем массив в строку
+    // $stringDifference = implode($stringDifference);// преобразуем массив в строку
 
-    $finalView = "{\n{$stringDifference}}\n";//финальный вид вывода различия двух файлов
+    // $finalView = "{\n{$stringDifference}}\n";//финальный вид вывода различия двух файлов
 
-    echo($finalView);
-    return $finalView;
+    // echo($finalView);
+    // return $finalView;
+
+    $diff = differ($firstParserResult, $secondParserResult);
+
+    var_export($diff);
+
+    $jsonresult = json_encode($diff);
+    file_put_contents('result.json', $jsonresult);
+
+
+
+
 }
